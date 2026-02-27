65373352dde7555732185a496a0d50c40e88417a
PallaviPattanashetti <Pallavi.Pattanashetti@gmail.com>
2026-02-27 10:40:12 -0800

    azure

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
+using blogapi.Services;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<BlogItemService>();
+builder.Services.AddScoped<PasswordService>();
+builder.Services.AddScoped<UserService>();
+
+var connectionString = builder.Configuration.GetConnectionString("myBlogString2");
+builder.Services.AddDbContext<DataContext>(Option => Option.UseSqlServer(connectionString));
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
